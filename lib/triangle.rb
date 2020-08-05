9992fabca0d3227afb9121c4f8cddb10bbc0bff5
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-05 20:56:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,12 +6,13 @@ class Triangle
   
   def initialize(first_side,second_side,third_side)
     @sides = first_side , second_side , third_side
-    binding.pry
+    
   end
   
   def kind
-    if @side_1 == @side_2 == @side_3
-      :equilateral
-    elsif
+    
   end
+  
 end
+
+class 
