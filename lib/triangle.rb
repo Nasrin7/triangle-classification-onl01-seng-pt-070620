7f0357e05d333f106bef41bfffc9af7a6f563885
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-05 20:54:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   attr_reader :sides
   
   def initialize(first_side,second_side,third_side)
-    @sides = 
+    @sides = first_side , second_side , third_side
   end
   
   def kind
