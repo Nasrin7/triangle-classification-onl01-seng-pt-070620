903aafeb83e0ead74e1fb5b7053f79c42e03e542
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-05 20:54:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,17 @@
+require 'pry'
+
 class Triangle
   
   attr_reader :sides
   
   def initialize(first_side,second_side,third_side)
     @sides = first_side , second_side , third_side
+    binding.pry
   end
   
-  def kind
-    if @side_1 == @side_2 == @side_3
-      :equilateral
-    elsif
-  end
+  # def kind
+  #   if @side_1 == @side_2 == @side_3
+  #     :equilateral
+  #   elsif
+  # end
 end
